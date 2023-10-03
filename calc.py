d86a7cc1708c6400f999ffe241028a48860f2952
Rostislav Kashpurenko <rkashpurenko@gmail.com>
2023-10-03 18:33:04 +0300

    Dobavil pervoe zadanie

---
+++ calc.py
@@ -0,0 +1,11 @@
+chislo1 = int(input("Введите первое число: "))
+action = input("Введите действие: ")
+chislo2 = int(input("Введите второе число: "))
+if action == "+" :  
+    print (chislo1 + chislo2)
+if action == "-" :
+    print (chislo1 - chislo2)
+if action == "*" :
+    print (chislo1 * chislo2)
+if action == "/" :
+    print (chislo1 / chislo2)
