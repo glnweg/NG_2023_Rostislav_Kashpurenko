e574ff627ba02b4ebe81868090a33f1147624038
Rostislav Kashpurenko <rkashpurenko@gmail.com>
2023-10-07 12:04:36 +0300

    Sdelal vse main tasks

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,5 +1,5 @@
 chislo1 = int(input("Vvedite pervoe chislo: "))
-action = input("Vvedite deistvie (+,-,*,/,**,): ")
+action = input("Vvedite deistvie (+,-,*,/,**,koren'): ")
 chislo2 = int(input("Vvedite vtoroe chislo: "))
 if action == "+" :  
     print (chislo1 + chislo2)
@@ -11,5 +11,7 @@ elif action == "/" :
     print (chislo1 / chislo2)
 elif action == "**" :
     print (chislo1 ** chislo2)
+elif action == "koren'" :
+    print (chislo1**(1/chislo2))
 else :
     print("Error")
