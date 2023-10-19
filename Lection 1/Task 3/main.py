57d7c7914abd00d1611a1bab2c75bb9098e1498f
Rostislav Kashpurenko <rkashpurenko@gmail.com>
2023-10-19 17:30:11 +0300

    Edited task 3 lection 1 and task 1 lection 2

--- Lection 1/Task 3/main.py
+++ Lection 1/Task 3/main.py
@@ -1,9 +1,8 @@
-gradusi = input ("1 or 2? (1 - Celsius to Fahrenheit, 2 - Fahrenheit to Celsius): ")
-if gradusi == "1":
-    celsius = int(input ("How many Celsius?\n"))
-    print ((celsius * 9/5) + 32 )
-elif gradusi == "2":
-    fahrenheit = int(input ("How many Fahrenheits?\n"))
-    print ((fahrenheit - 32) * 5/9)
+gradusi = int(input ("How many degrees?\n"))
+option = input ("1 or 2? (1 - Celsius to Fahrenheit, 2 - Fahrenheit to Celsius): ")
+if option == "1":
+    print ((gradusi * 9/5) + 32 )
+elif option == "2":
+    print ((gradusi - 32) * 5/9)
 else:
     print ("Error")
--- Lection 2/Task 1.py
+++ Lection 2/Task 1.py
@@ -1,8 +1,3 @@
-new_set =  set()
-print("Enter random stuff(/ to end):\n")
-elmnts = input("\n")
-while elmnts != "/" :
-    elmnts = input("\n")
-    new_set.add(elmnts)
-new_set.remove("/")
-print(new_set)
+elems = set()
+elems.update(input('Enter elements by spaces:\n').split())
+print (elems)
